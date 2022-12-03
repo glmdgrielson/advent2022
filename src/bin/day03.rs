bd2dffc4c2af9ec149c29458bc88b79670445cb5
glmdgrielson <zapperchamp1@gmail.com>
2022-12-03 11:21:59 -0500

    Document Day 3's solution

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -2,14 +2,20 @@ use std::collections::HashSet;
 use std::io::stdin;
 
 fn main() {
+    // This is a horrifying hack that only works because every puzzle
+    // takes input from a file. I'd love to know a better way of doing this.
     let lines = stdin().lines();
     let lines: Vec<_> = lines.filter(|l| l.is_ok()).map(|l| l.unwrap()).collect();
     let mut priority = 0;
     'search: for sack in lines.clone() {
         let (upper, lower) = sack.split_at(sack.len() / 2);
         for item in upper.chars() {
+            // If this were Python, I could just do `item in lower`,
+            // but Rust doesn't think that's safe or necessary. Welp.
             if let Some(_) = lower.find(item) {
                 priority += priority_value(item);
+                // This shortcuts the search. After all, there should only ever
+                // be one item shared between the two halves.
                 continue 'search;
             }
         }
@@ -21,8 +27,12 @@ fn main() {
         for elf in set {
             let chars = elf.chars();
             if *elf == first {
+                // If this is the first run through, just dump everything
+                // in the list of candidates.
                 badge_set = chars.collect();
             } else {
+                // Otherwise filter everything in the list of candidates
+                // that doesn't exist in this elf's sack.
                 let items: HashSet<char> = chars.collect();
                 for item in badge_set.clone() {
                     if !items.contains(&item) {
@@ -31,10 +41,13 @@ fn main() {
                 }
             }
         }
+        // If everything went okay, there should only ever be one item
+        // in the set at this point.
         assert!(badge_set.len() == 1, "We found counterfeit badges: {:?}", badge_set);
         let badges: Vec<_> = badge_set.into_iter().collect();
         badge_priority += priority_value(badges[0]);
     }
+    // Print the solutions to the puzzles.
     println!("Final sum of priorities is {}", priority);
     println!("Final sum of badge priority is {}", badge_priority);
 }
