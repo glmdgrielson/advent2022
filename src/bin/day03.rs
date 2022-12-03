be8a256475e965b678ae84f91c952e3899d42a2e
glmdgrielson <zapperchamp1@gmail.com>
2022-12-03 12:58:23 -0500

    Remove a terrible hack
    So, initially I couldn't figure out how to get the second part to play
    nicely with the same iterator from the first part. Then I realized I
    could just stuff them into a temporary Vec and run code when it was long
    enough. Tada!

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -17,56 +17,60 @@ use std::collections::HashSet;
 use std::io::stdin;
 
 fn main() {
-    // This is a horrifying hack that only works because every puzzle
-    // takes input from a file. I'd love to know a better way of doing this.
     let lines = stdin().lines();
-    let lines: Vec<_> = lines.filter(|l| l.is_ok()).map(|l| l.unwrap()).collect();
     let mut priority = 0;
-    'search: for sack in lines.clone() {
-        let (upper, lower) = sack.split_at(sack.len() / 2);
-        for item in upper.chars() {
-            // If this were Python, I could just do `item in lower`,
-            // but Rust doesn't think that's safe or necessary. Welp.
-            if let Some(_) = lower.find(item) {
-                priority += priority_value(item);
-                // This shortcuts the search. After all, there should only ever
-                // be one item shared between the two halves.
-                continue 'search;
-            }
-        }
-    }
     let mut badge_priority = 0;
-    // This divides the elves into triplets. `chunks` is a helpful
-    // function. I just wish the borrow checker was less annoying.
-    for set in lines.chunks(3) {
-        let first = set[0].clone();
-        let mut badge_set: HashSet<char> = HashSet::new();
-        for elf in set {
-            let chars = elf.chars();
-            if *elf == first {
-                // If this is the first run through, just dump everything
-                // in the list of candidates.
-                badge_set = chars.collect();
-            } else {
-                // Otherwise filter everything in the list of candidates
-                // that doesn't exist in this elf's sack.
-                let items: HashSet<char> = chars.collect();
-                for item in badge_set.clone() {
-                    if !items.contains(&item) {
-                        badge_set.remove(&item);
+    let mut triplet = vec![];
+    'search: for line in lines {
+        if let Ok(sack) = line {
+            // Prep for part 2.
+            triplet.push(sack.clone());
+            // Check to see if we have a triplet.
+            if triplet.len() == 3 {
+                // Even though this code is part 2, it has to be run first
+                // since success on the first part goes back to the start
+                // of the loop.
+                let first = &triplet[0];
+                let mut badge_set: HashSet<char> = HashSet::new();
+                for elf in triplet.iter() {
+                    let chars = elf.chars();
+                    if elf == first {
+                        // If this is the first run through, just dump everything
+                        // in the list of candidates.
+                        badge_set = chars.collect();
+                    } else {
+                        // Otherwise filter everything in the list of candidates
+                        // that doesn't exist in this elf's sack.
+                        let items: HashSet<char> = chars.collect();
+                        for item in badge_set.clone() {
+                            if !items.contains(&item) {
+                                badge_set.remove(&item);
+                            }
+                        }
                     }
+                }
+                // If everything went okay, there should only ever be one item
+                // in the set at this point.
+                assert!(
+                    badge_set.len() == 1,
+                    "We found counterfeit badges: {:?}",
+                    badge_set
+                );
+                let badges: Vec<_> = badge_set.into_iter().collect();
+                badge_priority += priority_value(badges[0]);
+            }
+            let (upper, lower) = sack.split_at(sack.len() / 2);
+            for item in upper.chars() {
+                // If this were Python, I could just do `item in lower`,
+                // but Rust doesn't think that's safe or necessary. Welp.
+                if let Some(_) = lower.find(item) {
+                    priority += priority_value(item);
+                    // This shortcuts the search. After all, there should only ever
+                    // be one item shared between the two halves.
+                    continue 'search;
                 }
             }
         }
-        // If everything went okay, there should only ever be one item
-        // in the set at this point.
-        assert!(
-            badge_set.len() == 1,
-            "We found counterfeit badges: {:?}",
-            badge_set
-        );
-        let badges: Vec<_> = badge_set.into_iter().collect();
-        badge_priority += priority_value(badges[0]);
     }
     // Print the solutions to the puzzles.
     println!("Final sum of priorities is {}", priority);
