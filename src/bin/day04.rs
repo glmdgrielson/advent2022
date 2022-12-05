d9709724fcd37748c5d9b1bfb18f2c09821f98f6
glmdgrielson <zapperchamp1@gmail.com>
2022-12-05 12:27:16 -0500

    Solve Part 2
    I take back what I said about Part 1 being more complicated.

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -7,6 +7,10 @@
 //! ------
 //! Find out how many pairs have redundant ranges. These would be pairs where
 //! one elf's range is completely contained by the other elf.
+//!
+//! Part 2
+//! ------
+//! Find out how many pairs contain any sort of overlap.
 
 use std::cmp::Ordering;
 use std::io::stdin;
@@ -14,7 +18,8 @@ use std::io::stdin;
 fn main() {
     let lines = stdin().lines();
     // Aaaaaaaaand guess who forgot to make the counter variable mutable? Oops.
-    let mut counter = 0;
+    let mut contains_count = 0;
+    let mut overlap_count = 0;
     for line in lines {
         if let Ok(pair) = line {
             // `this` should be the first elf. `that` is the second elf.
@@ -48,35 +53,58 @@ fn main() {
             } else {
                 panic!("Elf is slacking off!")
             };
-            match this.0.cmp(&that.0) {
-                Ordering::Less => {
-                    // The start of the first range is before the start
-                    // of the second range.
-                    if this.1 >= that.1 {
-                        // The end of the first range is _after_ the start
-                        // of the second range, so it counts.
-                        counter += 1;
-                        println!("Pairs are {:?} and {:?}", this, that);
-                    }
-                }
-                Ordering::Equal => {
-                    // The start of this range is exactly equal
-                    // the start of the second range. Therefore, one elf
-                    // containing another should be guaranteed?
-                    counter += 1;
-                    println!("Pairs are {:?} and {:?}", this, that);
-                }
-                Ordering::Greater => {
-                    // The start of this range is _inside_ the start of the
-                    // other range. Ergo, we need to check if this range is
-                    // smaller.
-                    if this.1 <= that.1 {
-                        counter += 1;
-                        println!("Pairs are {:?} and {:?}", this, that);
-                    }
-                }
+            if contains(this, that) {
+                contains_count += 1;
+            }
+            if overlaps(this, that) {
+                overlap_count += 1;
             }
         }
     }
-    println!("The number of poorly planned elf pairs is {}", counter);
+    println!(
+        "The number of poorly planned elf pairs is {}",
+        contains_count
+    );
+    println!("The number of unneeded collaborations is {}", overlap_count);
+}
+
+fn contains(this: (i32, i32), that: (i32, i32)) -> bool {
+    match this.0.cmp(&that.0) {
+        Ordering::Less => {
+            // The start of the first range is before the start
+            // of the second range.
+            // The end of the first range is _after_ the start
+            // of the second range, so it counts.
+            this.1 >= that.1
+        }
+        Ordering::Equal => {
+            // The start of this range is exactly equal
+            // the start of the second range. Therefore, one elf
+            // containing another should be guaranteed?
+            true
+        }
+        Ordering::Greater => {
+            // The start of this range is _inside_ the start of the
+            // other range. Ergo, we need to check if this range is
+            // smaller.
+            this.1 <= that.1
+        }
+    }
+}
+
+/// Determine whether one range overlaps another range.
+///
+/// This checks for the criteria for Part 2 of the puzzle.
+///
+/// I stole this solution from elsewhere, so I'm not _entirely_
+/// sold on the logic of it?
+fn overlaps(this: (i32, i32), that: (i32, i32)) -> bool {
+    // Check whether the start of `this` overlaps with the end of `that`.
+    if this.0 <= that.1 {
+        // Now reverse the check to make sure yes, we're dealing with
+        // the same numbers.
+        that.0 <= this.1
+    } else {
+        false
+    }
 }
