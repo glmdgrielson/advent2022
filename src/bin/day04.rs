36f9bced7fe9e35b92ef29a590538521cbcd1a21
glmdgrielson <zapperchamp1@gmail.com>
2022-12-05 08:46:24 -0500

    Start work on Day 4's puzzle
    I'm late because I was off to a show yesterday and was away from my
    computer. Twas a good time!

---
+++ src/bin/day04.rs
@@ -0,0 +1,20 @@
+//! Day 4's Advent of Code puzzle
+//! =============================
+//! Puzzle input consists of pairs of elves and the areas they have been
+//! assigned.
+//!
+//! Part 1
+//! ------
+//! Find out how many pairs have redundant ranges. These would be pairs where
+//! one elf's range is completely contained by the other elf.
+
+use std::io::stdin;
+
+fn main() {
+    let lines = stdin().lines();
+    let counter = 0;
+    for line in lines {
+        // 
+    }
+    println!("The number of poorly planned elf pairs is {}", counter);
+}
