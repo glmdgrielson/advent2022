248dd8f88c8e1e8b5408620ff409ad3a7d21cd81
glmdgrielson <zapperchamp1@gmail.com>
2022-12-05 14:25:52 -0500

    Prep for Day 5's puzzle
    This is gonna be a FUN one to parse.

---
+++ src/bin/day05.rs
@@ -0,0 +1,11 @@
+//! Day 5's Advent of Code puzzle
+//! -----------------------------
+//! I hate this one already.
+//!
+//! Puzzle input consists of a diagram of stacks as well as a list of
+//! instructions. The actual logic itself is a simple Tower of Hanoi thing.
+//! This is just gonna be a parsing headache.
+
+fn main() {
+    todo!("Oh holy night, I hate this already.")
+}
