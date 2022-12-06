e8433cc99eef9696cd15a031b092be97d9518eb3
glmdgrielson <zapperchamp1@gmail.com>
2022-12-06 11:03:59 -0500

    Solve Day 6 part 1
    Stupid off by one error.

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -1,7 +1,33 @@
 /// Day 6's Advent of Code puzzle
 /// =============================
-use std::io::stdin;
+/// For the first time, puzzle input is a single stream of characters. Oh no.
+///
+/// Part 1
+/// ------
+/// Find the index of the first four characters that are unique.
+use std::io::{stdin, Read};
 
 fn main() {
-	todo!()
+	let bytes = stdin().bytes();
+	let mut marker: Vec<_> = vec![];
+	let mut idx = 0;
+	for (i, byte) in bytes.enumerate() {
+		if let Ok(ch) = byte {
+			marker.push(ch);
+			if marker.len() == 4 {
+				let mut set = marker.clone();
+				set.sort();
+				set.dedup();
+				if set.len() == 4 {
+					idx = i + 1;
+					break;
+				}
+				marker.remove(0);
+			}
+			// idx += 1;
+		} else {
+			panic!("Welp, your input has failed. Oops!");
+		}
+	}
+	println!("The first marker {:?} can be found at index {}", marker, idx);
 }
