df445b2678074ae1fd5fe6437925efd6345e1144
glmdgrielson <zapperchamp1@gmail.com>
2022-12-06 13:31:09 -0500

    Solve Day 6 part 2
    Here's to hoping that `slice_take` stabilizes one of these days...

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -7,27 +7,45 @@
 /// Find the index of the first four characters that are unique.
 use std::io::{stdin, Read};
 
+const PACKET_SIZE: usize = 4;
+const MESSAGE_SIZE: usize = 14;
+
 fn main() {
 	let bytes = stdin().bytes();
 	let mut marker: Vec<_> = vec![];
-	let mut idx = 0;
+	let mut packet = 0;
+	let mut message = 0;
+	let mut part1 = false;
+	let mut part2 = false;
 	for (i, byte) in bytes.enumerate() {
 		if let Ok(ch) = byte {
 			marker.push(ch);
-			if marker.len() == 4 {
-				let mut set = marker.clone();
-				set.sort();
-				set.dedup();
-				if set.len() == 4 {
-					idx = i + 1;
-					break;
+			if marker.len() >= PACKET_SIZE && !part1 {
+				part1 = is_window_unique(&marker, PACKET_SIZE);
+				if part1 {
+					packet = i + 1;
 				}
-				marker.remove(0);
 			}
-			// idx += 1;
+			if marker.len() >= MESSAGE_SIZE && !part2 {
+				part2 = is_window_unique(&marker, MESSAGE_SIZE);
+				if part2 {
+					message = i + 1;
+				}
+			}
+			if part1 && part2 {
+				break;
+			}
 		} else {
 			panic!("Welp, your input has failed. Oops!");
 		}
 	}
-	println!("The first marker {:?} can be found at index {}", marker, idx);
+	println!("The packet marker can be found at index {}", packet);
+	println!("The message marker can be found at index {}", message);
+}
+
+fn is_window_unique(marker: &[u8], size: usize) -> bool {
+	let mut set = marker.iter().rev().take(size).collect::<Vec<_>>();
+	set.sort();
+	set.dedup();
+	set.len() == size
 }
