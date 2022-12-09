3c52e64cf0d2e4375aaba0f51016c29e34acf113
glmdgrielson <zapperchamp1@gmail.com>
2022-12-09 17:59:02 -0500

    Flesh out Day 10's skeleton
    Is this necessary? No, I don't even know the puzzle yet.
    Do I want to? Sure, why not.

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -1,10 +1,27 @@
 /// Day 10's Advent of Code puzzle
 /// ==============================
 
-use advent::Advent;
+use advent::{Advent, input_to_str};
 
+#[derive(Debug)]
 struct Day10;
 
+impl Advent for Day10 {
+    type Answer1 = ();
+
+    type Answer2 = ();
+
+    fn parse_input(input: &str) -> Self {
+        todo!()
+    }
+
+    fn part_one(&self) -> Self::Answer1 {
+        todo!()
+    }
+}
+
 fn main() {
-	todo!()
+	let input = input_to_str();
+	let code = Day10::parse_input(&input);
+	code.part_one();
 }
