e980c02808846b2767b62faa9c2e4895ea0f1b2f
glmdgrielson <zapperchamp1@gmail.com>
2022-12-09 15:47:41 -0500

    Adjust `Advent` trait
    I don't really need a separate type for the output of parsing. I already
    have A data type, because this trait has to be implemented somewhere.

--- src/advent.rs
+++ src/advent.rs
@@ -1,15 +1,19 @@
 pub trait Advent {
-	/// The parsed results of the input file.
-	type Data;
 	/// The output desired for Part one of the puzzle.
 	type Answer1;
 	/// The output desired for Part 2 of the puzzle.
 	type Answer2;
 
-	fn parse_input(input: &str) -> Self::Data;
-	fn part_one(data: Self::Data) -> Self::Answer1;
-	fn part_two(data: Self::Data) -> Self::Answer2 {
-		Self::part_one(data);
+	/// Convert input (from stdin, perhaps) into a usable data format.
+	fn parse_input(input: &str) -> Self;
+	/// Solve the first part of the puzzle.
+	fn part_one(&self) -> Self::Answer1;
+	/// Solve part two of the puzzle.
+	///
+	/// This is a separate function because the first part's
+	/// result is useless for part 2. Have fun rewriting that code.
+	fn part_two(&self) -> Self::Answer2 {
+		self.part_one();
 		todo!("Part 2's puzzle is not yet known, give us a minute.");
 	}
 }
