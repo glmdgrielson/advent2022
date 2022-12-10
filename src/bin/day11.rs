2e717637a8a35489005f1ad77730da5814a7c237
glmdgrielson <zapperchamp1@gmail.com>
2022-12-10 12:47:52 -0500

    Prep for Day 11's puzzle.

---
+++ src/bin/day11.rs
@@ -0,0 +1,25 @@
+/// Day 11's Advent of Code puzzle
+/// ==============================
+
+use advent::{Advent, input_to_str};
+
+struct Day11;
+
+impl Advent for Day11 {
+    type Answer1 = ();
+
+    type Answer2 = ();
+
+    fn parse_input(input: &str) -> Self {
+        todo!()
+    }
+
+    fn part_one(&self) -> Self::Answer1 {
+        todo!()
+    }
+}
+
+fn main() {
+	let data = Day11::parse_input(&input_to_str());
+	data.part_one();
+}
