ce93a1891230135a467fc0ed704bdecc94288b56
glmdgrielson <zapperchamp1@gmail.com>
2022-12-31 15:47:06 -0500

    Solve Part 2 of Day 21
    A little refactoring did the trick! Huzzah!

--- src/bin/day21.rs
+++ src/bin/day21.rs
@@ -5,7 +5,7 @@
 //! Part 1
 //! ------
 //! What does the `root` monkey evaluate to?
-//! 
+//!
 //! Part 2
 //! ------
 //! We are `humn`. What do we need to report so that both of the monkeys
@@ -89,7 +89,22 @@ impl Advent for Day21 {
 		team.entry("humn".to_owned()).and_modify(|m| {
 			*m = Monkey::Unknown;
 		});
-		todo!()
+		let root = self.team.get("root").expect("Missing root!");
+		match root {
+			Monkey::Operation { op: _, left, right } => {
+				let left = team.get(left).expect("Missing partner!");
+				let right = team.get(right).expect("Missing partner!");
+				let one = left.try_evaluate(&team);
+				let two = right.try_evaluate(&team);
+				match (one, two) {
+					(Some(_), Some(_)) => panic!("Where's the human?"),
+					(None, Some(target)) => left.equal_target(&team, target),
+					(Some(target), None) => right.equal_target(&team, target),
+					(None, None) => panic!("Too many humans!"),
+				}
+			}
+			_ => panic!("Invalid root!"),
+		}
 	}
 }
 
@@ -117,14 +132,122 @@ impl Monkey {
 					Operation::Mul => left.evaluate(map) * right.evaluate(map),
 					Operation::Div => left.evaluate(map) / right.evaluate(map),
 				}
-			},
+			}
 			Monkey::Unknown => panic!("Human stupid"),
 		}
 	}
 
+	/// Tries to evaluate this monkey. If the human is found somewhere,
+	/// it returns [`None`].
 	fn try_evaluate(&self, map: &HashMap<String, Monkey>) -> Option<i64> {
-		// 
-		todo!()
+		if self.has_unknown_input(map) {
+			match self {
+				Monkey::Integer(_) => {
+					unreachable!("Constant evaluation failed!")
+				}
+				Monkey::Operation { op, left, right } => {
+					let left = map.get(left).expect("Missing partner!");
+					let right = map.get(right).expect("Missing partner!");
+					let one = left.try_evaluate(map);
+					let two = right.try_evaluate(map);
+					match (one, two) {
+						(Some(one), Some(two)) => match op {
+							Operation::Add => Some(one + two),
+							Operation::Sub => Some(one - two),
+							Operation::Mul => Some(one * two),
+							Operation::Div => Some(one / two),
+						},
+						(None, Some(_)) => None,
+						(Some(_), None) => None,
+						(None, None) => {
+							unreachable!("Extraneous humans found!")
+						}
+					}
+				}
+				Monkey::Unknown => None,
+			}
+		} else {
+			Some(self.evaluate(map))
+		}
+	}
+
+	fn has_unknown_input(&self, map: &HashMap<String, Monkey>) -> bool {
+		match self {
+			Monkey::Integer(_) => false,
+			Monkey::Operation { op: _, left, right } => {
+				let left = map.get(left).expect("Missing partner!");
+				let right = map.get(right).expect("Missing partner!");
+				left.has_unknown_input(map) || right.has_unknown_input(map)
+			}
+			Monkey::Unknown => true,
+		}
+	}
+
+	/// Attempt to resolve this monkey into equaling the target value.
+	fn equal_target(&self, map: &HashMap<String, Monkey>, target: i64) -> i64 {
+		match self {
+			Monkey::Integer(num) => *num,
+			Monkey::Operation { op, left, right } => {
+				let left = map.get(left).expect("Missing partner!");
+				let right = map.get(right).expect("Missing partner!");
+				let one = left.try_evaluate(map);
+				let two = right.try_evaluate(map);
+				match op {
+					Operation::Add => {
+						// target = constant + unknown
+						if let Some(num) = one {
+							assert!(two.is_none());
+							right.equal_target(map, target - num)
+						} else if let Some(num) = two {
+							left.equal_target(map, target - num)
+						} else {
+							panic!("Monkey haystack failure!")
+						}
+					}
+					Operation::Sub => {
+						if let Some(num) = one {
+							// target = constant - unknown
+							// Ergo, unknown = constant - target
+							assert!(two.is_none());
+							right.equal_target(map, num - target)
+						} else if let Some(num) = two {
+							// target = unknown - constant
+							// Ergo, unknown = target + constant
+							left.equal_target(map, target + num)
+						} else {
+							panic!("Monkey haystack failure!")
+						}
+					}
+					Operation::Mul => {
+						// target = constant * unknown
+						// Ergo, unknown = target / constant
+						if let Some(num) = one {
+							assert!(two.is_none());
+							right.equal_target(map, target / num)
+						} else if let Some(num) = two {
+							left.equal_target(map, target / num)
+						} else {
+							panic!("Monkey haystack failure!")
+						}
+					},
+					Operation::Div => {
+						if let Some(num) = one {
+							// target = constant / unknown
+							// Ergo, unknown = constant / target
+							assert!(two.is_none());
+							right.equal_target(map, num / target)
+						} else if let Some(num) = two {
+							// target = unknown / constant
+							// Ergo, unknown = target * constant
+							left.equal_target(map, target * num)
+						} else {
+							panic!("Monkey haystack failure!")
+						}
+					},
+				}
+			}
+			Monkey::Unknown => target,
+		}
 	}
 }
 
@@ -140,4 +263,19 @@ fn main() {
 	let runner = Day21::parse_input(&input_to_str());
 	println!("Root monkey says {}", runner.part_one());
 	println!("Human monkey needs to say {}", runner.part_two());
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn test_part_two() {
+		use advent::get_example_input;
+		let example = get_example_input("src/input/day21-example.txt");
+		let runner = Day21::parse_input(&example);
+
+		assert_eq!(runner.part_two(), 301)
+		// let runner = 
+	}
 }
